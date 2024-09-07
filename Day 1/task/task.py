7f88f70b70c9ded2cb3ae23c2a038bd502517020
j-vilchez <114251972+j-vilchez@users.noreply.github.com>
2024-09-07 08:29:47 -0400

    This is a band name generation to practice string manipulation

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,2 +1,5 @@
-#Just a simple Hello World
-print("Hello World!")
+#This is a band name generation to practice string manipulation
+print("Welcome to the Band Name generator!" )
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name is:" + " " + city + " " + pet)
